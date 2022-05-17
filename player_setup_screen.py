8f8baf14b1617a67f5fc1040b4b9972a54a1f747
Lucka <luckaflidrova@gmail.com>
2022-05-17 14:48:47 +0200

    submit buttons removed from player setup screen
    //still no save tho

--- player_setup_screen.py
+++ player_setup_screen.py
@@ -15,20 +15,17 @@ class PlayerSetupScreen:
         self.name_text = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((100, 100), (100, 50)),
                                                          text="Your name:", manager=self.ui_manager)
         self.player_name_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 100), (100, 50)), manager=self.ui_manager)
-        self.player_name_submit = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 100), (150, 50)),text='Submit', manager=self.ui_manager)
 
         self.surname_text = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((80, 200), (120, 50)),
                                                          text="Your surname:", manager=self.ui_manager)
         self.player_surname_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 200), (100, 50)), manager=self.ui_manager)
-        self.player_surname_submit = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 200), (150, 50)),
-                                                               text='Submit', manager=self.ui_manager)
 
         self.next_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((200, 300), (150, 50)),
                                                                   text='Next', manager=self.ui_manager)
         self.bg = (pygame.transform.scale(pygame.image.load("background.jpg"), self.screen_options.resolution))
         #self.player_surname_input.text = self.game_settings.player_surname
-        self.content = [[self.welcome_text], [self.name_text, self.player_name_input, self.player_name_submit],
-                        [self.surname_text, self.player_surname_input, self.player_surname_submit], [self.next_button]]
+        self.content = [[self.welcome_text], [self.name_text, self.player_name_input],
+                        [self.surname_text, self.player_surname_input], [self.next_button]]
         self.hide()
 
     def refresh(self):
@@ -56,26 +53,19 @@ class PlayerSetupScreen:
             for item in line_list:
                 item.show()
 
-    def _on_click_name_submit(self):
-        if self.player_name_input.text == "":
-            pass
-        else:
-            self.game_settings.player_name = self.player_name_input.get_text()
-
-    def _on_click_surname_submit(self):
-        if self.player_surname_input.text == "":
-            pass
-        else:
-            self.game_settings.player_surname = self.player_surname_input.get_text()
 
     def _on_click_next(self):
+        if self.player_name_input.text == "":
+            return
+        else:
+            self.game_settings.player_name = self.player_name_input.get_text()
+        if self.player_surname_input.text == "":
+            return
+        else:
+            self.game_settings.player_surname = self.player_surname_input.get_text()
         self.screen_options.show(self.screen_options.choice_screen)
 
     def process_event(self, event):
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == self.player_name_submit:
-                self._on_click_name_submit()
-            if event.ui_element == self.player_surname_submit:
-                self._on_click_surname_submit()
             if event.ui_element == self.next_button:
                 self._on_click_next()
